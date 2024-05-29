e26399837ad3bf130e6dd1516a90a8a76b2b72c7
Ashwin117 <ashwin.sundar117@gmail.com>
2024-05-29 00:13:40 -0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { Box } from "@chakra-ui/react";
 import CodeEditor from "./components/CodeEditor";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
-      <CodeEditor />
-    </Box>
-  );
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/">
+      <Route path="sandbox" element={
+        <Box minH="100vh" bg="#0f0a19" color="gray.500" px={6} py={8}>
+          <CodeEditor />
+        </Box>
+      }/>
+    </Route>
+  )
+)
+
+function App({routes}) {
+  return <RouterProvider router={router}/>
 }
 
 export default App;
